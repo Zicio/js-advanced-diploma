f317c0dd7abbb2b4088f701671915a7dffec2761
Alexander Burachkov <zicio@yandex.ru>
2021-08-15 18:34:45 +0400

    Task №5 Started

--- src/js/generators.js
+++ src/js/generators.js
@@ -5,7 +5,9 @@
  * @param maxLevel max character level
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
+
 export function* characterGenerator(allowedTypes, maxLevel) {
+  yield allowedTypes[Math.trunc(Math.random() * allowedTypes.length)];
   // TODO: write logic here
 }
 
